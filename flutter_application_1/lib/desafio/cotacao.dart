3e4954321d8bc9fd5ff61651ad12285f8d5d53d9
Amanda Victoria <amandamelquiades01@gmail.com>
2022-03-24 20:50:37 -0300

    mudança cotacao

--- flutter_application_1/lib/desafio/cotacao.dart
+++ flutter_application_1/lib/desafio/cotacao.dart
@@ -1,4 +1,35 @@
+import 'package:bloc_state_management/desafio/result.dart';
 import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Tela2(),
+//       debugShowCheckedModeBanner: false,
+//     ),
+//   );
+// }
+
+// class Tela2 extends StatelessWidget {
+//   const Tela2({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(255, 31, 31, 31),
+//       appBar: AppBar(
+//         backgroundColor: Colors.black,
+//         title: const Text(
+//           'Cotação',
+//           style: TextStyle(color: Colors.blue),
+//         ),
+//         centerTitle: true,
+//         elevation: 0,
+//       ),
+//       body: const Cotacao(),
+//     );
+//   }
+// }
 
 class Cotacao extends StatefulWidget {
   const Cotacao({Key? key}) : super(key: key);
@@ -20,66 +51,99 @@ class _CotacaoState extends State<Cotacao> {
                 color: const Color(0xFF2C2C2C),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8)),
-                child: const ListTile(
-                  leading: Icon(
-                    Icons.attach_money,
-                    size: 37,
+                child: const SizedBox(
+                  height: 70,
+                  child: Center(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.attach_money,
+                        size: 37,
+                      ),
+                      iconColor: Color(
+                        0xFFABB0AD,
+                      ),
+                      title: Text('Real',
+                          style: TextStyle(color: Color(0xFFABB0AD))),
+                    ),
                   ),
-                  iconColor: Color(0xFFABB0AD),
-                  title: Text('Real'),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(10.0),
               child: Card(
                 color: const Color(0xFF2C2C2C),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8)),
-                child: const ListTile(
-                  leading: Icon(
-                    Icons.attach_money,
-                    size: 37,
+                child: const SizedBox(
+                  height: 70,
+                  child: Center(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.attach_money,
+                        size: 37,
+                      ),
+                      iconColor: Color(
+                        0xFFABB0AD,
+                      ),
+                      title: Text('Libra Esterlina',
+                          style: TextStyle(color: Color(0xFFABB0AD))),
+                    ),
                   ),
-                  iconColor: Color(0xFFABB0AD),
-                  title: Text('Libra Estrelina'),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(10.0),
               child: Card(
                 color: const Color(0xFF2C2C2C),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8)),
-                child: const ListTile(
-                  leading: Icon(
-                    Icons.attach_money,
-                    size: 37,
+                child: const SizedBox(
+                  height: 70,
+                  child: Center(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.attach_money,
+                        size: 37,
+                      ),
+                      iconColor: Color(
+                        0xFFABB0AD,
+                      ),
+                      title: Text('Iene',
+                          style: TextStyle(color: Color(0xFFABB0AD))),
+                    ),
                   ),
-                  iconColor: Color(0xFFABB0AD),
-                  title: Text('Iene'),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(10.0),
               child: Card(
                 color: const Color(0xFF2C2C2C),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8)),
-                child: const ListTile(
-                  leading: Icon(
-                    Icons.attach_money,
-                    size: 37,
+                child: const SizedBox(
+                  height: 70,
+                  child: Center(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.attach_money,
+                        size: 37,
+                      ),
+                      iconColor: Color(
+                        0xFFABB0AD,
+                      ),
+                      title: Text('Peso Argentino',
+                          style: TextStyle(color: Color(0xFFABB0AD))),
+                    ),
                   ),
-                  iconColor: Color(0xFFABB0AD),
-                  title: Text('Peso Argentino'),
                 ),
               ),
             ),
           ],
         ),
+        const Resultado()
       ],
     );
   }
