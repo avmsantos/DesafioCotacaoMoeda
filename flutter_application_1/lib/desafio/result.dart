92e1adf9e5f2951bd63ef723151078413bead088
Amanda Victoria <amandamelquiades01@gmail.com>
2022-03-28 23:48:19 -0300

    mudança para list builder

--- flutter_application_1/lib/desafio/result.dart
+++ flutter_application_1/lib/desafio/result.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_state_management/desafio/class/reposotory.dart';
 import 'package:flutter/material.dart';
 
 class Resultado extends StatefulWidget {
@@ -10,6 +11,37 @@ class Resultado extends StatefulWidget {
 class _ResultadoState extends State<Resultado> {
   @override
   Widget build(BuildContext context) {
-    return const Center();
+    final tabela3 = TerceiraTela.tabela3;
+    return PageView(children: [
+      ListView.builder(
+        itemCount: tabela3.length,
+        itemBuilder: (BuildContext context, int moeda) {
+          return Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SizedBox(
+              height: 80,
+              child: Card(
+                color: const Color(0xFF2C2C2C),
+                margin: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.attach_money,
+                    size: 37,
+                  ),
+                  iconColor: const Color(0xFFABB0AD),
+                  title: Text(
+                    tabela3[moeda].nome,
+                    style: const TextStyle(color: Color(0xFFABB0AD)),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ]);
   }
 }
