1ef9240de92830a041bbde190df461158ab764bf
Amanda Victoria <amandamelquiades01@gmail.com>
2022-03-28 23:48:44 -0300

    classe moedas

---
+++ flutter_application_1/lib/desafio/class/moedas.dart
@@ -0,0 +1,23 @@
+class Moeda {
+  String nome;
+
+  Moeda({
+    required this.nome,
+  });
+}
+
+class MoedaSecond {
+  String nome;
+
+  MoedaSecond({
+    required this.nome,
+  });
+}
+
+class Resultado {
+  String nome;
+
+  Resultado({
+    required this.nome,
+  });
+}
