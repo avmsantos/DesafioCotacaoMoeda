6896258d05d6b8f920b5b986d36e120a7f2e2bba
Amanda Victoria <amandamelquiades01@gmail.com>
2022-04-04 19:09:34 -0300

    separação do runapp

---
+++ flutter_application_1/lib/desafio/main.dart
@@ -0,0 +1,11 @@
+import 'package:bloc_state_management/desafio/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
